886915d292e14eb2c42f1128b480c4d0f47c5bbd
allicsander <me@evergoal.ru>
2020-03-03 07:43:06 +0300

    first test quote

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuotesGrid.vue';
+
     export default {
+        data() {
+          return{
+              quotes: ['be sexy'],
+              maxQoutes: 10
+          } 
         
+        },
+        components:{
+          appQuoteGrid: QuoteGrid
+        }
+
     }
 </script>
 
