c939ceea04865bf8cc4388a7fdd2f8d3d8f0e0d6
allicsander <me@evergoal.ru>
2020-03-03 08:37:27 +0300

    stuff for adding new quote

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
     <div class="container">
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
       <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuotesGrid.vue';
+import NewQuote from './components/NewQuote.vue';
 
     export default {
         data() {
           return{
-              quotes: ['be sexy'],
+              quotes: ['be sexy', 'be strong'],
               maxQoutes: 10
           } 
         
         },
+        methods:{
+          newQuote(quote){
+            this.quotes.push(quote);
+          }
+        },
         components:{
-          appQuoteGrid: QuoteGrid
+          appQuoteGrid: QuoteGrid,
+          appNewQuote: NewQuote
         }
 
     }
